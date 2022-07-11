2e7a084cc0af08bfa4b6a66c3c419d21bc718ac1
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2022-07-11 19:13:34 +0400

    added utility functions for detecting active item

--- src/components/PackagesSection/UI/SVGLines.tsx
+++ src/components/PackagesSection/UI/SVGLines.tsx
@@ -7,29 +7,44 @@ export type Props = {
   activePackage: string | undefined;
 };
 
-
 const SVGLines = ({ position, activeFeature, activePackage }: Props) => {
-
   const getActiveLine = (features: string[]) => {
-    let str:string = position;
-    
-    if(position === 'left'){
-      if(features[2] === '3') str='third-'+ str;
-      if(features[2] === '2') str='second-'+ str;
-      if(features[2] === '1') str='first-'+ str;
+    let str: string = position;
+
+    if (position === "left") {
+      if (features[2] === "3") str = "third-" + str;
+      if (features[2] === "2") str = "second-" + str;
+      if (features[2] === "1") str = "first-" + str;
     }
 
-    if(position === 'right'){
-      if(features[2] === '3') str='first-'+ str;
-      if(features[2] === '2') str='second-'+ str;
-      if(features[2] === '1') str='third-'+ str;
+    if (position === "right") {
+      if (features[2] === "3") str = "first-" + str;
+      if (features[2] === "2") str = "second-" + str;
+      if (features[2] === "1") str = "third-" + str;
     }
 
     return str;
-  } 
+  };
+
+  const isActiveLine = (
+    activeFeature: Props["activeFeature"],
+    activePackage: Props["activePackage"],
+    number: string
+  ) => {
+    let activePositionNumber = position === "left" ? "1" : "2";
+    if (
+      activePackage === activeFeature[0] &&
+      activeFeature[1] === activePositionNumber &&
+      activeFeature[2] === number
+    ) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
-    <LinesContainer {...{ position, activeFeature, activePackage }}>
+    <LinesContainer {...{ position }}>
       <svg
         height="100%"
         width="100%"
@@ -37,22 +52,30 @@ const SVGLines = ({ position, activeFeature, activePackage }: Props) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <g id={`first-${position}`}>
-          <FirstPath
-            {...{ position, activeFeature, activePackage }}
+          <Path
+            isActive={isActiveLine(
+              activeFeature,
+              activePackage,
+              position === "left" ? "1" : "3"
+            )}
             d="m1,42 h40 a16,16 0 0 1 16,16 v66 m16,16 a16,16 0 0 1 -16,-16 m16,16 h48"
             fill="transparent"
           />
         </g>
         <g id={`second-${position}`}>
-          <SecondPath
-            {...{ position, activeFeature, activePackage }}
+          <Path
+            isActive={isActiveLine(activeFeature, activePackage, "2")}
             d="m0,140 h200"
             fill="transparent"
           />
         </g>
         <g id={`third-${position}`}>
-          <ThirdPath
-            {...{ position, activeFeature, activePackage }}
+          <Path
+            isActive={isActiveLine(
+              activeFeature,
+              activePackage,
+              position === "left" ? "3" : "1"
+            )}
             d="m58,156 a16,16 0 0 1 16,-16 h48 m-64,16 v66 a16,16 0 0 1 -16,16 m-40,0 h40"
             fill="transparent"
           />
@@ -63,9 +86,9 @@ const SVGLines = ({ position, activeFeature, activePackage }: Props) => {
   );
 };
 
-const LinesContainer = styled.div<Props>`
+const LinesContainer = styled.div<{ position: Props["position"] }>`
   width: 200px;
-  ${({ position, activeFeature, activePackage }) => {
+  ${({ position }) => {
     if (position === "right") {
       return css`
         transform: rotate(-180deg);
@@ -74,92 +97,14 @@ const LinesContainer = styled.div<Props>`
   }}
 `;
 
-const FirstPath = styled.path<Props>`
+const Path = styled.path<{ isActive: boolean }>`
   stroke: ${({ theme }) => theme.colors.purpleLight};
   stroke-width: 2px;
-  ${({ position, activeFeature, activePackage }) => {
-    if (position === "left") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "1" &&
-        activeFeature[2] === "1"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
-    }
-
-    if (position === "right") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "2" &&
-        activeFeature[2] === "3"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
-    }
-  }}
-`;
-
-const SecondPath = styled.path<Props>`
-  stroke: ${({ theme }) => theme.colors.purpleLight};
-  stroke-width: 2px;
-  ${({ position, activeFeature, activePackage }) => {
-    if (position === "left") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "1" &&
-        activeFeature[2] === "2"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
-    }
-
-    if (position === "right") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "2" &&
-        activeFeature[2] === "2"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
-    }
-  }}
-`;
-
-const ThirdPath = styled.path<Props>`
-  stroke: ${({ theme }) => theme.colors.purpleLight};
-  stroke-width: 2px;
-  ${({ position, activeFeature, activePackage }) => {
-    if (position === "left") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "1" &&
-        activeFeature[2] === "3"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
-    }
-
-    if (position === "right") {
-      if (
-        activePackage === activeFeature[0] &&
-        activeFeature[1] === "2" &&
-        activeFeature[2] === "1"
-      ) {
-        return css`
-          stroke: ${({ theme }) => theme.colors.violetSoft};
-        `;
-      }
+  ${({ isActive }) => {
+    if (isActive) {
+      return css`
+        stroke: ${({ theme }) => theme.colors.violetSoft};
+      `;
     }
   }}
 `;
