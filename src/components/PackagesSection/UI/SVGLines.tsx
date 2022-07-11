59bdce8d14eea2e85585ef9b33d7f11f37e74e25
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2022-07-11 15:58:42 +0400

    added curly lines implementation with svg

---
+++ src/components/PackagesSection/UI/SVGLines.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+export type Props = {
+  position: "left" | "right";
+  activeFeature: string[];
+  activePackage: string | undefined;
+};
+
+const SVGLines = ({ position, activeFeature, activePackage }: Props) => {
+
+  return (
+    <LinesContainer {...{ position, activeFeature, activePackage }}>
+      <svg
+        height="100%"
+        width="100%"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <FirstPath
+          {...{ position, activeFeature, activePackage }}
+          d="m1,42 h40 a16,16 0 0 1 16,16 v66 m16,16 a16,16 0 0 1 -16,-16 m16,16 h48"
+          fill="transparent"
+          stroke-width="2"
+        />
+        <SecondPath
+          {...{ position, activeFeature, activePackage }}
+          d="m0,140 h200"
+          fill="transparent"
+          stroke-width="2"
+        />
+        <ThirdPath
+          {...{ position, activeFeature, activePackage }}
+          d="m58,156 a16,16 0 0 1 16,-16 h48 m-64,16 v66 a16,16 0 0 1 -16,16 m-40,0 h40"
+          fill="transparent"
+          stroke-width="2"
+        />
+      </svg>
+    </LinesContainer>
+  );
+};
+
+const LinesContainer = styled.div<Props>`
+  width: 200px;
+  ${({ position, activeFeature, activePackage }) => {
+    if (position === "right") {
+      return css`
+        transform: rotate(-180deg);
+      `;
+    }
+  }}
+`;
+
+const FirstPath = styled.path<Props>`
+  stroke: ${({ theme }) => theme.colors.purpleLight};
+  ${({ position, activeFeature, activePackage }) => {
+    if (position === "left") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "1" &&
+        activeFeature[2] === "1"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+
+    if (position === "right") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "2" &&
+        activeFeature[2] === "3"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+  }}
+`;
+
+const SecondPath = styled.path<Props>`
+  stroke: ${({ theme }) => theme.colors.purpleLight};
+  ${({ position, activeFeature, activePackage }) => {
+    if (position === "left") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "1" &&
+        activeFeature[2] === "2"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+
+    if (position === "right") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "2" &&
+        activeFeature[2] === "2"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+  }}
+`;
+
+const ThirdPath = styled.path<Props>`
+      stroke: ${({ theme }) => theme.colors.purpleLight};
+  ${({ position, activeFeature, activePackage }) => {
+    if (position === "left") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "1" &&
+        activeFeature[2] === "3"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+
+    if (position === "right") {
+      if (
+        activePackage === activeFeature[0] &&
+        activeFeature[1] === "2" &&
+        activeFeature[2] === "1"
+      ) {
+        return css`
+          stroke: ${({ theme }) => theme.colors.violetSoft};
+        `;
+      }
+    }
+  }}
+`;
+
+export default SVGLines;
